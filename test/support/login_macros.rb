5abee0212b365020f9bef92cfe8b203f6ef45555
Henrique Morato <riquemorato@gmail.com>
2021-03-26 14:31:28 -0300

    Support files

---
+++ test/support/login_macros.rb
@@ -0,0 +1,5 @@
+module LoginMacros
+  def login_user(user = User.create!(email: 'jane.doe@iugu.com.br', password: '123456'))
+    login_as user, scope: :user
+  end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,8 +2,12 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative "../config/environment"
 require "rails/test_help"
 
+Dir[Rails.root.join('test/support/**/*.rb')].each { |f| require f  }
+
 class ActiveSupport::TestCase
   include Warden::Test::Helpers
+  include LoginMacros
+
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
 
@@ -13,8 +17,4 @@ class ActiveSupport::TestCase
   # Add more helper methods to be used by all tests here...
   Minitest.load_plugins
   Minitest::PrideIO.pride!
-
-  def login_user(user = User.create!(email: 'jane.doe@iugu.com.br', password: '123456'))
-    login_as user, scope: :user
-  end
 end
