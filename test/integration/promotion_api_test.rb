ef50e278fa24ec6e00c56ee4515fca9e6fb7afe7
Henrique Morato <riquemorato@gmail.com>
2021-03-31 17:10:37 -0300

    WIP começando api de show do cupom

---
+++ test/integration/promotion_api_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class PromotionApiTest < ActionDispatch::IntegrationTest
+  test 'show coupon' do
+    user = User.create!(email: 'test@iugu.com.br', password: '123456')
+    promotion = Promotion.create!(name: 'Natal',
+                                  description: 'Promoção de natal',
+                                  code: 'NATAL10', discount_rate: 15,
+                                  coupon_quantity: 5, 
+                                  expiration_date: '22/12/2033', user: user)
+    coupon = Coupon.create!(code: 'NATAL10-0001', promotion: promotion)
+
+    get "/api/v1/coupons/#{coupon.code}"
+
+    assert_response :success
+  end
+end
